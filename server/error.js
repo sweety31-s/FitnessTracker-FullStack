05131ab05db5954a5c507c8782be48b5414d8858
sweety31-s <51282363+sweety31-s@users.noreply.github.com>
2024-05-13 12:53:06 +0530

    Add files via upload

---
+++ server/error.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const createError = (status, message) => {
+    const err = new Error();
+    err.status = status;
+    err.message = message;
+    return err;
+  };
+  
